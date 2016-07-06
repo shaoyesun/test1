19b4d7d6e5d17ec95e078e3d39878947a502b4ef
sunjf <sunjf@biomarker.com.cn>
2016-07-06 11:20:55 +0800

    jjj

--- src/main/java/com/controller/TestController.java
+++ src/main/java/com/controller/TestController.java
@@ -6,6 +6,21 @@ import net.sf.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by sunjf on 2016/6/26.
  */
@@ -23,4 +38,68 @@ public class TestController {
         return "success";
     }
 
+    public static void main(String[] args) {
+        HttpServletResponse response = null;
+        try {
+            // path是指欲下载的文件的路径。
+            File file = new File("http://e.vhall.com/export/fcode/828356316");
+            // 取得文件名。
+            String filename = file.getName();
+            // 取得文件的后缀名。
+            String ext = filename.substring(filename.lastIndexOf(".") + 1).toUpperCase();
+
+            // 以流的形式下载文件。
+            InputStream fis = new BufferedInputStream(new FileInputStream("http://e.vhall.com/export/fcode/828356316"));
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            fis.close();
+            // 清空response
+            response.reset();
+            // 设置response的Header
+            response.addHeader("Content-Disposition", "attachment;filename=" + new String(filename.getBytes()));
+            response.addHeader("Content-Length", "" + file.length());
+            OutputStream toClient = new BufferedOutputStream(response.getOutputStream());
+            response.setContentType("application/octet-stream");
+            toClient.write(buffer);
+            toClient.flush();
+            toClient.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            // read file content from file
+            StringBuffer sb= new StringBuffer("");
+
+            FileReader reader = new FileReader("/home/sunjf/webinar_fcode_323151277_201607040851.csv");
+            BufferedReader br = new BufferedReader(reader);
+
+            String str = null;
+
+            while((str = br.readLine()) != null) {
+                sb.append(str+"/n");
+
+                System.out.println(str);
+            }
+
+            br.close();
+            reader.close();
+
+            // write string to file
+            /*FileWriter writer = new FileWriter("c://test2.txt");
+            BufferedWriter bw = new BufferedWriter(writer);
+            bw.write(sb.toString());
+
+            bw.close();
+            writer.close();*/
+        }
+        catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
