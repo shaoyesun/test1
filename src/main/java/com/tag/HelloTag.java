1baa78aae4d6fa11ab35080940d96ec77b2877fa
sunjf <sunjf@biomarker.com.cn>
2016-07-30 23:10:18 +0800

    result

--- src/main/java/com/tag/HelloTag.java
+++ src/main/java/com/tag/HelloTag.java
@@ -1,7 +1,19 @@
 package com.tag;
 
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+
 /**
  * Created by Administrator on 2016/7/11.
  */
-public class HelloTag {
+public class HelloTag extends SimpleTagSupport {
+
+    @Override
+    public void doTag() throws JspException, IOException {
+        JspWriter out = getJspContext().getOut();
+        out.println("Hello Custom Tag!");
+        System.out.println("wo shi zhong guo ren !");
+    }
 }
