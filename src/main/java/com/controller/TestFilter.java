e1670b5ba890ff7adcb22db3398d5b0abe93caa7
sunjf <sunjf@biomarker.com.cn>
2016-08-11 16:31:42 +0800

    123

---
+++ src/main/java/com/controller/TestFilter.java
@@ -0,0 +1,35 @@
+package com.controller;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by admin on 16-6-3.
+ */
+public class TestFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession();
+
+        httpResponse.sendRedirect("/test1");
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+
+    }
+}
