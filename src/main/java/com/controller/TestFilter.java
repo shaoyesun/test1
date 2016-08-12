55e2907db99e14e619fa18564de51b7a4288b96e
sunjf <sunjf@biomarker.com.cn>
2016-08-12 18:14:21 +0800

    china

--- src/main/java/com/controller/TestFilter.java
+++ src/main/java/com/controller/TestFilter.java
@@ -36,6 +36,8 @@ public class TestFilter implements Filter {
             }
         }
 
+        System.out.println("china");
+
         if (isExcludedPage) {//在过滤url之外
             filterChain.doFilter(servletRequest, servletResponse);
             //httpResponse.sendRedirect("/test2");
